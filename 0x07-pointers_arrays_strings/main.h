b8b08086afe305ad9a3fd3ec1f525574aaa7a84c
Thembinkosi <thembisiso25@gmail.com>
2023-04-04 12:16:23 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strpn(char *s, char *accept);
+char *_strbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+#endif
